a17634522dcaf84af96a5c0d912cffde224d591d
therkels <jakob.therkelsen@gmail.com>
2025-11-05 08:02:46 -0500

    ignore venv

--- chat.py
+++ chat.py
@@ -3,7 +3,7 @@ import torch
 from generate import generate
 from transformers import AutoTokenizer, AutoModel
 
-
+torch.cuda.empty_cache()
 def chat():
     device = 'cuda'
     model = AutoModel.from_pretrained('GSAI-ML/LLaDA-8B-Instruct', trust_remote_code=True, torch_dtype=torch.bfloat16).to(device).eval()
